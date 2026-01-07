adf675b0c18e67eb39aa0b3e29f797168bfed6e3
nikunjjadav123 <dirensj@gmail.com>
2026-01-07 14:25:27 +0530

    Updated index file and .npmignore file updated

--- src/index.js
+++ src/index.js
@@ -2,10 +2,14 @@ import * as core from './arrays/core.js';
 import * as adv from './arrays/advanced.js';
 import * as date from './date/index.js';
 import { chain } from './arrays/chain.js';
+import * as smart_time from './smart-time/index.js';
 
 export const SUT_ = {
   ...core,
   ...adv,
   ...date,
+  ...smart_time,
   chain,
 };
+
+
